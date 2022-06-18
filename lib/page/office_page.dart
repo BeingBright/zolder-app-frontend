37879943b55bda33f9d8f885f4407108de9fe8a5
Brighton van den End <BrightonvandenEnd@gmail.com>
2022-06-18 10:53:46 +0200

    Update office_page.dart

--- lib/page/office_page.dart
+++ lib/page/office_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../widget/sidebar.dart';
+
+const String locationRoute = "Locations";
+
 class OfficePage extends StatefulWidget {
   const OfficePage({Key? key}) : super(key: key);
 
@@ -8,14 +12,63 @@ class OfficePage extends StatefulWidget {
 }
 
 class _OfficePageState extends State<OfficePage> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  String currentPage = locationRoute;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-          child: Text("Office Page"),
+        title:  Center(
+          child: Text(currentPage),
         ),
       ),
+      drawer: Sidebar(title: "Office Page", username: "username", buttons: [
+        ListTile(
+            title: Row(
+              children: const [
+                Icon(Icons.work_outline),
+                Spacer(),
+                Text(locationRoute),
+                Spacer(),
+                Icon(Icons.work_outline, color: Colors.transparent),
+              ],
+            ),
+            onTap: () {
+              moveToScreen(context, locationRoute);
+            }),
+      ]),
+      body: Navigator(
+        key: _navigatorKey,
+        initialRoute: locationRoute,
+        onGenerateRoute: getCurrentPage,
+      ),
+    );
+  }
+
+  void moveToScreen(BuildContext context, String route) {
+    _navigatorKey.currentState!.pushNamed(route);
+    Navigator.pop(context);
+    setState(() {
+      currentPage = route;
+    });
+  }
+
+  Route getCurrentPage(RouteSettings settings) {
+    late Widget page;
+    switch (settings.name) {
+      case locationRoute:
+        page = Text("data");
+        break;
+      default:
+        page = Text("OOPS");
+        break;
+    }
+
+    return MaterialPageRoute<Widget>(
+      builder: (context) => page,
+      settings: settings,
     );
   }
 }
