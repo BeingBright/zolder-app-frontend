3ff0051fb29746c72067cb0398a11de26be05e5a
Brighton van den End <BrightonvandenEnd@gmail.com>
2022-06-18 10:53:48 +0200

    Update worker_page.dart

--- lib/page/worker_page.dart
+++ lib/page/worker_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import '../widget/sidebar.dart';
+
+const String locationRoute = "Locations";
+
 class WorkerPage extends StatefulWidget {
   const WorkerPage({Key? key}) : super(key: key);
 
@@ -8,14 +12,63 @@ class WorkerPage extends StatefulWidget {
 }
 
 class _WorkerPageState extends State<WorkerPage> {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  String currentPage = locationRoute;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(
-          child: Text("Worker Page"),
+        title: Center(
+          child: Text(currentPage),
         ),
       ),
+      drawer: Sidebar(title: "Worker Page", username: "username", buttons: [
+        ListTile(
+            title: Row(
+              children: const [
+                Icon(Icons.work_outline),
+                Spacer(),
+                Text(locationRoute),
+                Spacer(),
+                Icon(Icons.work_outline, color: Colors.transparent),
+              ],
+            ),
+            onTap: () {
+              moveToScreen(context, locationRoute);
+            }),
+      ]),
+      body: Navigator(
+        key: _navigatorKey,
+        initialRoute: locationRoute,
+        onGenerateRoute: getCurrentPage,
+      ),
+    );
+  }
+
+  void moveToScreen(BuildContext context, String route) {
+    _navigatorKey.currentState!.pushNamed(route);
+    Navigator.pop(context);
+    setState(() {
+      currentPage = route;
+    });
+  }
+
+  Route getCurrentPage(RouteSettings settings) {
+    late Widget page;
+    switch (settings.name) {
+      case locationRoute:
+        page = Text("locaion");
+        break;
+      default:
+        page = Text("OOPS");
+        break;
+    }
+
+    return MaterialPageRoute<Widget>(
+      builder: (context) => page,
+      settings: settings,
     );
   }
 }
