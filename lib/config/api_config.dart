b2dd45c8a2c90bb0528afbe5043a76940db8af60
Brighton van den End <BrightonvandenEnd@gmail.com>
2022-06-19 15:36:36 +0200

    Update api_config.dart

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,5 @@
 class APIConfig {
-  static const baseURI = "http://localhost:7002/zolder";
+  static const baseURI = "http://brightonvandenend.nl:7000/zolder";
   static const user = "$baseURI/user";
   static const location = "$baseURI/location";
   static const auth = "$baseURI/auth";
