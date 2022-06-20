39c858b2753a10664e168b398cb555e2695d5730
Brighton van den End <BBT.vandenEnd@student.han.nl>
2022-06-20 13:54:56 +0200

    linked login page to auth backend

--- lib/configuration/api_configuration.dart
+++ lib/configuration/api_configuration.dart
@@ -1,7 +1,17 @@
 class APIConfiguration {
-  static const baseURI = "http://localhost:7000/zolder";
-  static const user = "$baseURI/user";
-  static const location = "$baseURI/location";
-  static const auth = "$baseURI/auth";
-  static const locationAuth = "$location/audit";
+  static const String baseURI = "http://localhost:7000/zolder";
+  static const String user = "$baseURI/user";
+  static const String location = "$baseURI/location";
+  static const String auth = "$baseURI/auth";
+  static const String locationAuth = "$location/audit";
+
+  static const Map<String, String> baseHeader = <String, String>{
+    'Content-Type': 'application/json; charset=UTF-8'
+  };
+
+  static Map<String, String> getHeadersWithToken(String token) {
+    Map<String, String> headers = baseHeader;
+    headers.putIfAbsent("token", () => token);
+    return headers;
+  }
 }
--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,10 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:zolder_app/configuration/api_configuration.dart';
+import 'package:zolder_app/models/server_exception.dart';
+
+import '../models/user.dart';
 import '../models/usertoken.dart';
 
 class AuthService {
@@ -7,14 +14,14 @@ class AuthService {
 
   AuthService._newInstance();
 
-  Future<UserToken> loginUser(String username, String password) {
-    return Future.delayed(
-      const Duration(seconds: 2),
-      () {
-        // throw Exception("OOPS");
-        return UserToken("token", "user", "worker");
-      },
-    );
+  Future<UserToken> loginUser(String username, String password) async {
+    var response = await http.post(Uri.parse(APIConfiguration.auth),
+        headers: APIConfiguration.baseHeader,
+        body: jsonEncode(User(null, username, password, null)));
+    if (response.statusCode == 200) {
+      return UserToken.fromJson(jsonDecode(response.body));
+    }
+    throw ServerException.fromJson(jsonDecode(response.body));
   }
 
   Future logoutUser() {
