89a9ccdfae378282db8476babdb3008e696e3973
Brighton van den End <BBT.vandenEnd@student.han.nl>
2022-06-21 10:42:53 +0200

    added scrolling login page

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -8,7 +8,10 @@ class LoginPage extends StatefulWidget {
 
   final double? elementPadding;
 
-  const LoginPage({Key? key, this.mainPadding, this.elementPadding})
+  final double? elementSize;
+
+  const LoginPage(
+      {Key? key, this.mainPadding, this.elementPadding, this.elementSize})
       : super(key: key);
 
   @override
@@ -24,117 +27,139 @@ class _LoginPageState extends State<LoginPage> {
 
   double _mainPadding = 16;
   double _elementPadding = 16;
+  double _elementSize = 350;
 
   @override
   void initState() {
     super.initState();
     _mainPadding = (widget.mainPadding) ?? _mainPadding;
     _elementPadding = (widget.elementPadding) ?? _elementPadding;
+    _elementSize = (widget.elementSize) ?? _elementSize;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(_mainPadding),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.all(_elementPadding),
-              child: Center(
-                child: Text("Zolder app",
-                    style: Theme.of(context).textTheme.headline3),
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(_mainPadding),
+            child: Center(
+              child: LayoutBuilder(
+                builder: (BuildContext context, BoxConstraints constraints) {
+                  if (constraints.maxWidth > _elementSize) {
+                    return SizedBox(
+                      width: _elementSize,
+                      child: Card(child: loginForm(context)),
+                    );
+                  } else {
+                    return loginForm(context);
+                  }
+                },
               ),
             ),
-            Padding(
-              padding: EdgeInsets.all(_elementPadding),
-              child: Center(
-                child: Text(
-                  "Login Page",
-                  style: Theme.of(context).textTheme.headline6,
-                ),
-              ),
-            ),
-            AutofillGroup(
-              key: _formKey,
-              onDisposeAction: AutofillContextAction.commit,
-              child: Form(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsets.all(_elementPadding),
-                    child: TextFormField(
-                      autofocus: true,
-                      controller: _usernameController,
-                      autofillHints: const [AutofillHints.username],
-                      keyboardType: TextInputType.name,
-                      onTap: () => _usernameController.selection =
-                          TextSelection(
-                              baseOffset: 0,
-                              extentOffset:
-                                  _usernameController.value.text.length),
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Username",
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(_elementPadding),
-                    child: TextFormField(
-                      controller: _passwordController,
-                      autofillHints: const [AutofillHints.password],
-                      obscureText: true,
-                      keyboardType: TextInputType.visiblePassword,
-                      onTap: () => _passwordController.selection =
-                          TextSelection(
-                              baseOffset: 0,
-                              extentOffset:
-                                  _passwordController.value.text.length),
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(),
-                        labelText: "Password",
-                      ),
-                    ),
-                  ),
-                ],
-              )),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(0),
-              child: Center(
-                child: Text(_errorMessage,
-                    style: Theme.of(context)
-                        .textTheme
-                        .headline6!
-                        .copyWith(color: Colors.red)),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(_elementPadding),
-              child: ElevatedButton(
-                onPressed: () => AuthCommand()
-                    .loginUser(context, _usernameController.text,
-                        _passwordController.text)
-                    .onError((error, stackTrace) {
-                  setState(() {
-                    _errorMessage = "Invalid User";
-                  });
-                  return UserToken.empty();
-                }),
-                child: Padding(
-                  padding: EdgeInsets.all(_elementPadding),
-                  child: const Text("Log in"),
-                ),
-              ),
-            )
-          ],
+          ),
         ),
       ),
+    );
+  }
+
+  Widget loginForm(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: EdgeInsets.all(_elementPadding),
+          child: Center(
+            child: Text("Zolder app",
+                style: Theme.of(context).textTheme.headline3),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(_elementPadding),
+          child: Center(
+            child: Text(
+              "Login Page",
+              style: Theme.of(context).textTheme.headline6,
+            ),
+          ),
+        ),
+        AutofillGroup(
+          key: _formKey,
+          onDisposeAction: AutofillContextAction.commit,
+          child: Form(
+              child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(_elementPadding),
+                child: TextFormField(
+                  autofocus: true,
+                  controller: _usernameController,
+                  autofillHints: const [AutofillHints.username],
+                  keyboardType: TextInputType.name,
+                  onTap: () => _usernameController.selection = TextSelection(
+                      baseOffset: 0,
+                      extentOffset: _usernameController.value.text.length),
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Username",
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(_elementPadding),
+                child: TextFormField(
+                  controller: _passwordController,
+                  autofillHints: const [AutofillHints.password],
+                  obscureText: true,
+                  keyboardType: TextInputType.visiblePassword,
+                  onTap: () => _passwordController.selection = TextSelection(
+                      baseOffset: 0,
+                      extentOffset: _passwordController.value.text.length),
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: "Password",
+                  ),
+                ),
+              ),
+            ],
+          )),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(0),
+          child: Center(
+            child: Text(_errorMessage,
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(color: Colors.red)),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(_elementPadding),
+          child: ElevatedButton(
+            onPressed: () => AuthCommand()
+                .loginUser(
+                    context, _usernameController.text, _passwordController.text)
+                .onError((error, stackTrace) {
+              setState(() {
+                _errorMessage = "Invalid User";
+              });
+              return UserToken.empty();
+            }),
+            child: Padding(
+              padding: EdgeInsets.all(_elementPadding),
+              child: const Text("Log in"),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:zolder_app/main.dart';
+import 'package:zolder_app/views/login_page.dart';
 
 void main() {
   testWidgets('Hello World', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    expect(find.byType(HomePage), findsOneWidget);
+    expect(find.byType(LoginPage), findsOneWidget);
   });
 }
