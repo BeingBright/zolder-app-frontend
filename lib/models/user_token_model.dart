a86591f93eb89748e4f34eb3b4e7a93ea2934448
Brighton van den End <BBT.vandenEnd@student.han.nl>
2022-06-22 09:10:48 +0200

    Update user_token_model.dart

--- lib/models/user_token_model.dart
+++ lib/models/user_token_model.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:zolder_app/models/user_token.dart';
 
 class UserTokenModel extends ChangeNotifier {
-  // UserToken userToken = UserToken.empty();
-  UserToken userToken = UserToken("Token", "User", "ADMIN");
+  UserToken userToken = UserToken.empty();
 
   void setToken(UserToken newToken) {
     userToken = newToken;
