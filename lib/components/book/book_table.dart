9afa99b14e7efcd7487cfe2ae25f653078bca984
Brighton van den End <BBT.vandenEnd@student.han.nl>
2022-06-30 10:10:30 +0200

    Update book_table.dart

--- lib/components/book/book_table.dart
+++ lib/components/book/book_table.dart
@@ -29,12 +29,17 @@ class BookTable extends StatefulWidget {
 class _BookTableState extends State<BookTable> {
   @override
   Widget build(BuildContext context) {
+    print("hehehehehehehehehehehehehe");
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: StickyHeadersTable(
+        key: UniqueKey(),
         rowsLength: widget.location.columnCount!,
         columnsLength: widget.location.rowCount!,
         legendCell: Text(widget.location.inventoryLocation!),
+        onContentCellPressed: (i,j){
+
+        },
         columnsTitleBuilder: (int columnIndex) => Text("Rij $columnIndex"),
         rowsTitleBuilder: (int rowIndex) => Text("$rowIndex"),
         cellDimensions: const CellDimensions.fixed(
@@ -53,55 +58,3 @@ class _BookTableState extends State<BookTable> {
     );
   }
 }
-
-/*
-
-import 'package:flutter/material.dart';
-import 'package:table_sticky_headers/table_sticky_headers.dart';
-import 'package:zolder_app/models/book.dart';
-import 'package:zolder_app/models/location.dart';
-
-import 'book_item.dart';
-
-class BookTable extends StatelessWidget {
-  BookTable({Key? key, required this.location}) : super(key: key) {
-    books2d = generateList(location);
-  }
-
-  final Location location;
-
-  late final List<List<Book?>> books2d;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10.0),
-      child: StickyHeadersTable(
-        rowsLength: location.columnCount!,
-        columnsLength: location.rowCount!,
-        legendCell: Text(location.inventoryLocation!),
-        columnsTitleBuilder: (int columnIndex) => Text("Rij $columnIndex"),
-        rowsTitleBuilder: (int rowIndex) => Text("Pallet $rowIndex"),
-        cellAlignments: CellAlignments.fixed(
-            contentCellAlignment: Alignment.center,
-            stickyColumnAlignment: Alignment.center,
-            stickyRowAlignment: Alignment.center,
-            stickyLegendAlignment: Alignment.center),
-        contentCellBuilder: (i, j) =>
-            BookItem(book: books2d[i][j]!, context: context),
-      ),
-    );
-  }
-
-  static List<List<Book?>> generateList(Location loc) {
-    List<List<Book?>> bookList = List.generate(loc.rowCount ?? 0,
-        (i) => List.generate(loc.columnCount ?? 0, (j) => null));
-    for (var b in loc.books) {
-      bookList[b.row!][b.column!] = b;
-    }
-    print(bookList);
-    return bookList;
-  }
-}
-
- */
